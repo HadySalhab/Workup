9458d687ad93a757c028190842f13ff71abfa667
Hady Salhab <hadysalhab1@gmail.com>
2020-07-22 18:55:23 +1000

    Server structure refactor

---
+++ services/passport.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/api/auth/google/callback",
+		},
+		(accessToken) => {
+			console.log("accessToken", accessToken);
+		}
+	)
+);
+module.exports = passport;
