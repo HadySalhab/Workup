64953d05a0a0bbd84ae9d38afaee82fe84ef6134
Hady Salhab <hadysalhab1@gmail.com>
2020-07-22 22:21:15 +1000

    update passport callback

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,21 @@ const User = require("../models/User");
 // @desc      Register,Login user with google oauth
 // @route     GET /api/v1/auth/google
 // @access    Public
-exports.passportCallback = (accessToken, refreshToken, profile, done) => {
-	new User({
-		googleId: profile.id,
-		name: profile.displayName,
-		photo: profile.photos[0].value,
-		email: profile.emails[0].value,
-	}).save();
+exports.passportCallback = async (accessToken, refreshToken, profile, done) => {
+	try {
+		const existingUser = await User.findOne({ googleId: profile.id });
+		if (existingUser) {
+			return done(null, existingUser);
+		}
+		const user = await new User({
+			googleId: profile.id,
+			name: profile.displayName,
+			photo: profile.photos[0].value,
+			email: profile.emails[0].value,
+		}).save();
+		done(null, user);
+	} catch (err) {
+		console.log(err);
+		done(err, null);
+	}
 };
