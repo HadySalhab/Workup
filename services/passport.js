38eaa3197ccfb3bbd1a81a6012a7ff1fb9083d95
Hady Salhab <hadysalhab1@gmail.com>
2020-07-22 22:49:14 +1000

    deserialize user

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,7 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const { passportCallback } = require("../controllers/auth");
 const keys = require("../config/keys");
+const User = require("../models/User");
 
 passport.use(
 	new GoogleStrategy(
@@ -16,6 +17,16 @@ passport.use(
 
 passport.serializeUser((user, done) => {
 	done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (err) {
+		console.log(err);
+		done(err, null);
+	}
 });
 
 module.exports = passport;
