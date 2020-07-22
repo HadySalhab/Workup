8534a668fa2d4c7e65453ee1ccb8431a24f37851
Hady Salhab <hadysalhab1@gmail.com>
2020-07-22 16:01:29 +1000

    Passport lib setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy();
 const app = express();
-
+passport.use(new GoogleStrategy());
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log("server listening on port " + PORT);
