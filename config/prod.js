bfba203962cb19f6e942412c5426fdf78751d5ac
Hady Salhab <hadysalhab1@gmail.com>
2020-07-23 14:12:48 +1000

    Split keys to prod and dev

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieExpire: process.env.COOKIE_EXPIRE,
+	cookieKey: process.env.COOKIE_KEY,
+};
